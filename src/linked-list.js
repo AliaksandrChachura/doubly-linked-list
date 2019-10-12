9bab7dcfa1e844b1722b004eb9b9215c98479dac
Aliaksandr Chachura <47753412+AliaksandrChachura@users.noreply.github.com>
2019-10-12 19:18:39 +0300

    Update linked-list.js

--- src/linked-list.js
+++ src/linked-list.js
@@ -2,40 +2,163 @@ const Node = require('./node');
 
 class LinkedList {
     constructor() {
-        this.head = null;
-        this.tail = null;
+        this._head = null;
+        this._tail = null;
+        this.length = 0;
     }
 
     append(data) {
-        let node = new Node (data);
-        if (!this.head) {
-            this.head = data;
-            this.tail = data;
+        const newNode = new Node (data);
+        if (this.length === 0) {
+            this._head = newNode;
+            this._tail = newNode;
         }
         else {
-            node.prev = this.tail;
-            this.tail.next = node;
-            this.tail = node;
+            newNode.prev = this._tail;
+            this._tail.next = newNode;
+            this._tail = newNode;
         }
+        this.length++;
     }
 
-    head() {}
+    head() {
+        return this._head.data;
+    }
 
-    tail() {}
+    tail() {
+        return this._tail.data;
+    }
 
-    at(index) {}
+    at(index) {
+        if (index < 0 || this.length <= index ) {
+            return null;
+          }
+    
+          let current = this._head;
+          let position = 0;
+    
+          while (position < index) {
+            current = current.next;
+            position++;
+          }
+    
+          return current.data;
+      }
+    
 
-    insertAt(index, data) {}
+    insertAt(index, data) {
+        if (index < 0 || this.length < index) {
+            return false;
+        }
+        let node = new Node (data);
 
-    isEmpty() {}
+        if (index === 0) {
+            node.next = this._head;
+            this._head.prev = node;
+            this._head = node;
+        }
+        else if (index === this.length) {
+            this._tail.next = node;
+            node.prev = this._tail;
+            this._tail = node;
+        }
+        else {
+            let current = this._head;
+            let prev = null;
+            let position = 0;
+            while (position < index) {
+                prev = current;
+                current = current.next;
+                position++;
+            }
+            prev.next = node;
+            node.prev = prev;
+            node.next = current;
+            current.prev = node;
+        }
+        this.length++;
+    }
 
-    clear() {}
+    isEmpty() {
+        return this.length === 0;
+    }
 
-    deleteAt(index) {}
+    clear() {        
+        const newNode = new Node (null);
+            this._head = newNode;
+            this._tail = newNode;
+            this.length = 0;
+    }
 
-    reverse() {}
+    deleteAt(index) {
+        if (index < 0 || this.length <= index ) {
+            return null;
+          }
+      
+          let current;
+    
+          if (index === 0) {
+            current = this._head;
+    
+            this._head = this._head.next;
+            this._head.prev = null;
+          } else if(index    === this.length - 1) {
+            current = this._tail;
+    
+            this._tail = this._tail.prev;
+            this._tail.next = null;
+          } else {
+            current = this._head;
+    
+            let prev = null;
+            let position = 0;
+      
+            while (position < index) {
+              prev = current;
+              current = current.next;
+              position++;
+            }
+      
+            prev.next = current.next;
+            current.next.prev = prev;
+          }
+      
+          this.length--;
+          return current.data;
+    }
 
-    indexOf(data) {}
-}
+    reverse() {
+        if (this.length === 1) {
+            throw new TypeError('TypeError: Cannot read property \'reverse\' of undefined');
+        }
+        let current = this._head;
+        let prev = null;
+        while( current ){
+            let next = current.next;
+            current.next = prev;
+            current.prev = next;
+            prev = current;
+            current = next;
+        }
+        this._tail = this._head;
+        this._head = prev;
+    }
+
+    indexOf(data) {
+        let current = this._head;
+        let index = 0;
+
+        while (current) {
+            if (current.data === data) {
+            return index;
+        }
+
+        current = current.next;
+        index++;
+      }
+
+      return -1;
+    }
+    }
 
 module.exports = LinkedList;
